2a8bbceab8810d46c72e9d0ed933f9eae51d908f
Yunus Emre Köklü <220202054@ogr.atu.edu.tr>
2025-07-28 15:38:02 +0300

    App.js Düzenlendi

--- view!/App.js
+++ view!/App.js
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Button 
-        title= "Increase"
-        touchSoundDisabled 
+      <Button
+        title="Increase"
+        touchSoundDisabled
         onPress={() => {
           setCount((pre) => pre + 3);
         }}
@@ -22,9 +22,12 @@ export default function App() {
         onPress={() => {
           setCount((prev) => prev - 1);
         }}
-        color={"green"}    
+        color={"green"}
       />
-      <Text style={{fontSize: 35 , color:"purple", fontWeight: "bold"}}> {count}  </Text>
+      <Text style={{ fontSize: 35, color: "purple", fontWeight: "bold" }}>
+        {" "}
+        {count}{" "}
+      </Text>
     </View>
   );
 }
@@ -32,8 +35,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
